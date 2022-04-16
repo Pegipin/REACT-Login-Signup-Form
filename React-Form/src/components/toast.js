364c8e16952dabd878131447c56c9f1941f5bee7
Pegipin <74768438+Pegipin@users.noreply.github.com>
2022-04-16 13:28:20 +0200

    Add files via upload

---
+++ React-Form/src/components/toast.js
@@ -0,0 +1,12 @@
+import { toast } from 'react-toastify';
+
+
+export const notify = (text,type) =>{
+ if(type==='success'){
+  toast.success(text )
+ }else{
+  if(type==='error'){
+   toast.error(text);
+  }
+ }
+}
